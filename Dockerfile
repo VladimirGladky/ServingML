122329361430de665ea795ac3dceb10cd4c482c8
VladimirGladky <vovagl23@mail.ru>
2025-05-27 03:17:18 +0300

    add dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,26 +1,35 @@
-FROM golang:1.21-alpine AS builder
-
-RUN apk add --no-cache git gcc musl-dev cmake make
+FROM golang:1.24 AS builder
 
 WORKDIR /app
 COPY go.mod go.sum ./
 RUN go mod download
+
+RUN wget https://github.com/daulet/tokenizers/releases/download/v1.20.2/libtokenizers.linux-amd64.tar.gz \
+    && tar -xzf libtokenizers.linux-amd64.tar.gz -C /usr/lib \
+    && rm libtokenizers.linux-amd64.tar.gz
+
 COPY . .
-COPY /usr/lib/libonnxruntime.so /usr/lib/libonnxruntime.so
+COPY docker_libs/libonnxruntime.so /usr/lib/libonnxruntime.so
+
+ENV CGO_LDFLAGS="-L/usr/lib -ltokenizers -lonnxruntime"
 
 RUN cd cmd/server && \
-    CGO_ENABLED=1 GOOS=linux go build -ldflags="-s -w" -o /server
+    CGO_ENABLED=1 GOOS=linux go build -o /server
 
-FROM alpine:3.18
+FROM debian:bookworm-slim
 
-RUN apk add --no-cache libstdc++
+RUN apt-get update && apt-get install -y libstdc++6 libgcc-s1
 
 COPY --from=builder /server /app/server
 COPY --from=builder /app/internal/modelWrapper/data /app/internal/modelWrapper/data
 COPY --from=builder /usr/lib/libonnxruntime.so /usr/lib/libonnxruntime.so
+COPY --from=builder /usr/lib/libtokenizers.a /usr/lib/libtokenizers.a
+COPY --from=builder /app/config/config.yaml /app/config/config.yaml
 
 ENV LD_LIBRARY_PATH=/usr/lib
 ENV ONNXRUNTIME_SHARED_LIB_PATH=/usr/lib/libonnxruntime.so
+
+RUN chmod +x /app/server
 
 WORKDIR /app
 EXPOSE 6060
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,22 +1,20 @@
 package config
 
 import (
-	"ServingML/pkg/postgres"
 	"github.com/ilyakaznacheev/cleanenv"
 )
 
 type Config struct {
-	Postgres            postgres.Config `yaml:"Postgres" env:"POSTGRES"`
-	GrpcPort            string          `yaml:"grpc_port" env:"GRPC_PORT" env-default:"6060"`
-	GrpcHost            string          `yaml:"grpc_host" env:"GRPC_HOST" env-default:"localhost"`
-	Model1TokenizerPath string          `yaml:"model1_tokenizer_path" env:"MODEL1_TOKENIZER_PATH" env-default:"/home/smooth/Рабочий стол/ServingML/internal/modelWrapper/data/rubert-tiny2-russian-sentiment-onnx/tokenizer.json"`
-	Model1Path          string          `yaml:"model1_model_path" env:"MODEL1_MODEL_PATH" env-default:"/home/smooth/Рабочий стол/ServingML/internal/modelWrapper/data/rubert-tiny2-russian-sentiment-onnx/model.onnx"`
-	Model1OutputSize    int             `yaml:"model1_output_size" env:"MODEL1_OUTPUT_SIZE" env-default:"3"`
-	Model1BatchSize     int             `yaml:"model1_batch_size" env:"MODEL1_BATCH_SIZE" env-default:"4"`
-	Model2TokenizerPath string          `yaml:"model2_tokenizer_path" env:"MODEL2_TOKENIZER_PATH" env-default:"/home/smooth/Рабочий стол/ServingML/internal/modelWrapper/data/rubert-tiny2-russian-emotion-detection-ru-go-emotions/tokenizer.json"`
-	Model2Path          string          `yaml:"model2_model_path" env:"MODEL2_MODEL_PATH" env-default:"/home/smooth/Рабочий стол/ServingML/internal/modelWrapper/data/rubert-tiny2-russian-emotion-detection-ru-go-emotions/model.onnx"`
-	Model2OutputSize    int             `yaml:"model2_output_size" env:"MODEL2_OUTPUT_SIZE" env-default:"28"`
-	Model2BatchSize     int             `yaml:"model2_batch_size" env:"MODEL2_BATCH_SIZE" env-default:"4"`
+	GrpcPort            string `yaml:"grpc_port" env:"GRPC_PORT" env-default:"6060"`
+	GrpcHost            string `yaml:"grpc_host" env:"GRPC_HOST" env-default:"localhost"`
+	Model1TokenizerPath string `yaml:"model1_tokenizer_path" env:"MODEL1_TOKENIZER_PATH" env-default:"/home/smooth/Рабочий стол/ServingML/internal/modelWrapper/data/rubert-tiny2-russian-sentiment-onnx/tokenizer.json"`
+	Model1Path          string `yaml:"model1_model_path" env:"MODEL1_MODEL_PATH" env-default:"/home/smooth/Рабочий стол/ServingML/internal/modelWrapper/data/rubert-tiny2-russian-sentiment-onnx/model.onnx"`
+	Model1OutputSize    int    `yaml:"model1_output_size" env:"MODEL1_OUTPUT_SIZE" env-default:"3"`
+	Model1BatchSize     int    `yaml:"model1_batch_size" env:"MODEL1_BATCH_SIZE" env-default:"4"`
+	Model2TokenizerPath string `yaml:"model2_tokenizer_path" env:"MODEL2_TOKENIZER_PATH" env-default:"/home/smooth/Рабочий стол/ServingML/internal/modelWrapper/data/rubert-tiny2-russian-emotion-detection-ru-go-emotions/tokenizer.json"`
+	Model2Path          string `yaml:"model2_model_path" env:"MODEL2_MODEL_PATH" env-default:"/home/smooth/Рабочий стол/ServingML/internal/modelWrapper/data/rubert-tiny2-russian-emotion-detection-ru-go-emotions/model.onnx"`
+	Model2OutputSize    int    `yaml:"model2_output_size" env:"MODEL2_OUTPUT_SIZE" env-default:"28"`
+	Model2BatchSize     int    `yaml:"model2_batch_size" env:"MODEL2_BATCH_SIZE" env-default:"4"`
 }
 
 func New() (*Config, error) {
